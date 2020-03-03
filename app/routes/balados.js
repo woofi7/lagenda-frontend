a943c3e2b5de6d37512759436a8cb526b19e523f
Nicolas Signori <n.signo@hotmail.com>
2020-03-03 03:39:05 -0500

    Remove bare strings

--- app/routes/balados.js
+++ app/routes/balados.js
@@ -16,7 +16,10 @@ export default Route.extend({
           postDatetime: '02-26-2020',
           updateDatetime: '02-26-2020',
           desc: 'Les courses à la chefferies se suivent, mais ne se ressemblent pas. On parle de la course du PQ, du PLQ et du Parti Conservateur. Trois courses, trois ambiances et surtout trois constats bien différents. ',
-          thumbnailUrl: 'assets/images/bg-1.jpg',
+          thumbnail: {
+            url: '/assets/images/bg-1.jpg',
+            alt: 'Description image.'
+          },
           appleLink: 'https://podcasts.apple.com/ca/podcast/courses-%C3%A0-la-chefferies/id1500688101?i=1000466815260',
           googlePlayLink: 'https://www.google.com/podcasts?feed=aHR0cHM6Ly9mZWVkcy5zaW1wbGVjYXN0LmNvbS9sTmFHMHNGUw%3D%3D',
           spotifyLink: 'https://open.spotify.com/episode/1GQ5lXdLq88tB03IAJDYto',
--- app/routes/index.js
+++ app/routes/index.js
@@ -18,7 +18,7 @@ export default class IndexRoute extends Route {
           title: 'Le balado',
           desc: 'Pour des analyses détaillées.',
           image: {
-            url: 'assets/images/balados-slider.jpg',
+            url: '/assets/images/balados-slider.jpg',
             alt: 'Microphone dans un studio d\'enregistrement.'
           },
         },
@@ -28,7 +28,7 @@ export default class IndexRoute extends Route {
           title: 'À propos',
           desc: 'Qui diable est assez cinglé pour se lancer dans ce projet?',
           image: {
-            url: 'assets/images/lagenda-2-slider.jpg',
+            url: '/assets/images/lagenda-2-slider.jpg',
             alt: 'Le parlement du Canada.'
           },
         },
@@ -38,7 +38,7 @@ export default class IndexRoute extends Route {
           title: 'La tribune',
           desc: 'L’endroit pour vous informer et vous exprimer.',
           image: {
-            url: 'assets/images/tribune-slider.jpg',
+            url: '/assets/images/tribune-slider.jpg',
             alt: 'Bureau avec divers objets.'
           },
         },
@@ -48,7 +48,7 @@ export default class IndexRoute extends Route {
           title: 'L\'Agenda',
           desc: 'Créer la politique de demain.',
           image: {
-            url: 'assets/images/lagenda-slider.jpg',
+            url: '/assets/images/lagenda-slider.jpg',
             alt: 'Château Frontenac à Québec.'
           },
         },
@@ -62,7 +62,7 @@ export default class IndexRoute extends Route {
           desc: 'Les courses à la chefferies se suivent, mais ne se ressemblent pas. On parle de la course du PQ, du' +
             ' PLQ et du Parti Conservateur. Trois courses, trois ambiances et surtout trois constats bien différents.',
           thumbnail: {
-            url: 'assets/images/bg-7.jpg',
+            url: '/assets/images/bg-7.jpg',
             alt: 'Image description'
           },
         },
@@ -74,7 +74,7 @@ export default class IndexRoute extends Route {
           desc: 'Un petit mot pour vous expliquer qui nous sommes, nos buts et ce à quoi vous pouvez vous attendre de' +
             ' la part de la Tribune de L’Agenda.',
           thumbnail: {
-            url: 'assets/images/bg-8.jpg',
+            url: '/assets/images/bg-8.jpg',
             alt: 'Image description'
           },
         },
@@ -86,7 +86,7 @@ export default class IndexRoute extends Route {
           desc: 'Depuis 2015 maintenant, Justin Trudeau est premier ministre du Canada et plusieurs décisions de sa' +
             ' part auront comme effet de lui tailler une place dans l’histoire politique canadienne.',
           thumbnail: {
-            url: 'assets/images/bg-8.jpg',
+            url: '/assets/images/bg-8.jpg',
             alt: 'Image description'
           },
         },
@@ -98,7 +98,7 @@ export default class IndexRoute extends Route {
           desc: 'Quelles sont les motivations qui nous animent et qui nous poussent à passer plusieurs heures par jours' +
             ' sur ce projet ambitieux?',
           thumbnail: {
-            url: 'assets/images/bg-9.jpg',
+            url: '/assets/images/bg-9.jpg',
             alt: 'Image description'
           },
         },
