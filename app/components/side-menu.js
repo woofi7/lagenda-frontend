fd63b89dcd99f49ad46a465abb1341776993162c
Nicolas Signori <n.signo@hotmail.com>
2020-03-02 21:02:22 -0500

    Working

--- app/components/side-menu.js
+++ app/components/side-menu.js
@@ -1,4 +1,15 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class SideMenuComponent extends Component {
+  @service screen;
+
+  @tracked collapsed = true;
+
+  @action
+  toggleItems() {
+    this.collapsed = !this.collapsed;
+  }
 }
